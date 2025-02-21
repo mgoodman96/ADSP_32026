c90adaf57b4abb3841e7fd84a6b788ece82a8fab
mgoodman96 <mgoodman96@gmail.com>
2025-02-20 21:53:35 -0600

    add log

--- score_headlines_api.py
+++ score_headlines_api.py
@@ -32,3 +32,23 @@ def score_headlines(headline: str):
     return {'score': headline_score}
 
 #py -m uvicorn score_headlines_api:app --host localhost --port 8080 --reload
+
+# INFO:     Application startup complete.
+# INFO:     75.86.52.153:61099 - "GET / HTTP/1.1" 404 Not Found
+# INFO:     75.86.52.153:61099 - "GET /status HTTP/1.1" 200 OK
+# INFO:     75.86.52.153:61105 - "GET /score_headlines?headlines=BAD HTTP/1.1" 422 Unprocessable Entity
+# Batches: 100%|█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 1/1 [00:00<00:00,  3.49it/s]
+# INFO:     75.86.52.153:61105 - "GET /score_headlines?headline=BAD HTTP/1.1" 200 OK
+# INFO:     75.86.52.153:64112 - "GET /status HTTP/1.1" 200 OK
+# INFO:     75.86.52.153:64112 - "GET /favicon.ico HTTP/1.1" 404 Not Found
+# Batches: 100%|█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 1/1 [00:00<00:00, 25.22it/s]
+# INFO:     75.86.52.153:61111 - "GET /score_headlines?headline=BAD HTTP/1.1" 200 OK
+# INFO:     216.183.125.156:42989 - "GET /status HTTP/1.1" 200 OK
+# INFO:     216.183.125.156:42989 - "GET /status HTTP/1.1" 200 OK
+# INFO:     216.183.125.156:42989 - "GET /status HTTP/1.1" 200 OK
+# INFO:     75.86.52.153:64146 - "GET /status HTTP/1.1" 200 OK
+# ^CINFO:     Shutting down
+# INFO:     Waiting for application shutdown.
+# INFO:     Application shutdown complete.
+# INFO:     Finished server process [423629]
+# INFO:     Stopping reloader process [423625]
